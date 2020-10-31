1d1b5068ae6d70fd68cf3dd614bc25c0ae4eb0c7
__Nairy__ <59345570+zNairy@users.noreply.github.com>
2020-10-31 01:05:23 -0300

    Update removeExifData.py

--- removeExifData.py
+++ removeExifData.py
@@ -59,5 +59,5 @@ class Exif(object):
 
 
 if __name__ == '__main__':
-    arquivos = Exif('/media/znairy/BACKUP/Backup/minhas fotos/2019/')
+    arquivos = Exif('your path of photos')
     arquivos.RemoveMetaData()
